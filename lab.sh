7821a5ceb9e06a8e2e8c029a01a757973a172df4
vdellaglio <37350979+vdellaglio@users.noreply.github.com>
2020-10-21 09:44:27 +0100

    Update lab.sh

--- lab.sh
+++ lab.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-# ssh key config
-mkdir /home/vinicius/.ssh
-cp authorized_keys /home/vinicius/.ssh
-cp authorized_keys /root/.ssh
-# custom vi
-cp .vimrc /home/vinicius
-cp .vimrc /root
-# ssh config
-sed -i 's/#PermitRootLogin/PermitRootLogin/' /etc/ssh/sshd_config
-sed -i 's/#PubkeyAuthentication/PubkeyAuthentication/' /etc/ssh/sshd_config
+mkdir "/home/vinicius/.ssh"
