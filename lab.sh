f75063acc289fdda481648cc0aca9118d9ff22ef
vdellaglio <37350979+vdellaglio@users.noreply.github.com>
2020-10-21 09:15:31 +0100

    Update lab.sh

--- lab.sh
+++ lab.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
-cd /home/vinicius/labsetup
 # ssh key config
 mkdir /home/vinicius/.ssh/
-cp /home/vinicius/labsetup/authorized_keys /home/vinicius/.ssh/
-cp /home/vinicius/labsetup/authorized_keys /root/.ssh/
-# custom vi
-cp /home/vinicius/labsetup/.vimrc /home/vinicius/
-cp /home/vinicius/labsetup/.vimrc /root/
-# ssh config
-sed -i 's/#PermitRootLogin/PermitRootLogin/' /etc/ssh/sshd_config
-sed -i 's/#PubkeyAuthentication/PubkeyAuthentication/' /etc/ssh/sshd_config
-# update
-apt-get -y update
-# net tools
-apt-get -y install net-tools
-# update and upgrade
-apt-get -y update && apt-get -y upgrade
-# sudoers
-touch /etc/sudoers.d/90-sudo
-echo "vinicius ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/90-sudo
+cp authorized_keys /home/vinicius/.ssh/
+cp authorized_keys /root/.ssh/
 # custom vi
 cp .vimrc /home/vinicius/
 cp .vimrc /root/
-# rc.local
-cp start.sh /home/vinicius/
-chmod +x /home/vinicius/start.sh
-cp rc.local /etc/
-chown root /etc/rc.local
-chmod 755 /etc/rc.local
+# ssh config
+sed -i 's/#PermitRootLogin/PermitRootLogin/' /etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication/PubkeyAuthentication/' /etc/ssh/sshd_config
